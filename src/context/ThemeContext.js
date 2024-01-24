ea94836b818edb9bdc63034a0b8095f2c0e74d76
sapient-nirav <nirav.rathod@sapientcodelabs.xyz>
2024-01-24 17:13:30 +0530

    change

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,25 +1,3 @@
-// import React, { createContext, useState, useContext } from "react";
-
-// const ThemeContext = createContext();
-
-// export const ThemeProvider = ({ children }) => {
-//   const [isDarkMode, setDarkMode] = useState(false);
-
-//   const toggleDarkMode = () => {
-//     setDarkMode((prevMode) => !prevMode);
-//   };
-
-//   return (
-//     <ThemeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
-//       {children}
-//     </ThemeContext.Provider>
-//   );
-// };
-
-// export const useTheme = () => {
-//   return useContext(ThemeContext);
-// };
-
 import React, { createContext, useState, useContext } from "react";
 
 const ThemeContext = createContext();
--- src/context/UserContext.js
+++
@@ -1,4 +0,0 @@
-import React, { createContext } from "react";
-
-const UserContext = createContext();
-export default UserContext;
